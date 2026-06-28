9c05972cc05ace45fadaf84c302c9f77e283c46a
llcfernandes <lucasmividi@gmail.com>
2026-06-28 12:01:45 -0300

    feat: add image upload functionality for categories

--- src/routes.js
+++ src/routes.js
@@ -20,7 +20,7 @@ routes.post('/products', adminMiddleware,upload.single('file'), ProductControlle
 routes.put('/products/:id', adminMiddleware,upload.single('file'), ProductController.update);
 routes.get('/products', ProductController.index);
 
-routes.post('/categories', adminMiddleware,CategoryController.store);
+routes.post( '/categories', adminMiddleware, upload.single('file'), CategoryController.store);
 routes.get('/categories', CategoryController.index);
 
 export default routes;
