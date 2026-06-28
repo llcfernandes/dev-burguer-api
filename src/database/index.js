6326d840268319d0f9e364b541cd1f7086150873
llcfernandes <lucasmividi@gmail.com>
2026-06-28 13:07:29 -0300

    feat: setup MongoDB connection and create Order schema

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import { Sequelize } from 'sequelize';
+import mongoose from 'mongoose';
 import Product from '../app/models/Product.js';
 import User from '../app/models/User.js';
 import databaseConfig from '../config/database.cjs';
@@ -8,6 +9,7 @@ const models = [User, Product, Category];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
   init() {
     this.connection = new Sequelize(databaseConfig);
@@ -16,6 +18,12 @@ class Database {
       .map(
         (model) => model.associate && model.associate(this.connection.models),
       );
+  }
+
+  mongo() {
+    this.mongooseConnection = mongoose.connect(
+      'mongodb://localhost:27017/devburguer',
+    );
   }
 }
 
